2524041562408a6ccba9ea0c4a0d63c848f78cb5
Arinji2 <arinjaydhar205@gmail.com>
2025-01-26 00:01:42 +0530

    Setup Network Helper

--- network/network.go
+++ network/network.go
@@ -5,18 +5,48 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"net/url"
 )
 
-func MakeRequest[T any](url string, method string, body T) ([]byte, error) {
+func MakeRequest[T any](url *url.URL, method string, body T) ([]byte, error) {
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
 	}
-	req, err := http.NewRequest(method, url, bytes.NewReader(jsonBody))
+	req, err := http.NewRequest(method, url.String(), bytes.NewReader(jsonBody))
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseBody, nil
+}
+
+func MakeAuthenticatedRequest[T any](url *url.URL, method string, body T, authHeader string) ([]byte, error) {
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url.String(), bytes.NewReader(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", authHeader)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
