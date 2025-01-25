faf4697dbff8e48b5b5f9c6a06e807e1caf522b8
Arinji2 <arinjaydhar205@gmail.com>
2025-01-26 00:02:10 +0530

    Link Main with all Bot and Pocketbase entrypoints

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"log"
+
+	"github.com/arinji2/law-bot/bot"
 	"github.com/arinji2/law-bot/env"
+	"github.com/arinji2/law-bot/pb"
 )
 
 func main() {
 	e := env.SetupEnv()
-	// discordBot, err := bot.NewBot(e.Bot.Token, e.Bot.GuildID)
-	// if err != nil {
-	// 	log.Panicf("Cannot create bot: %v", err)
-	// }
-	// discordBot.Run()
+	pbAdmin := pb.SetupPocketbase(e.PB)
+
+	discordBot, err := bot.NewBot(e.Bot.Token, e.Bot.GuildID)
+	if err != nil {
+		log.Panicf("Cannot create bot: %v", err)
+	}
+	discordBot.Run(pbAdmin)
 }
